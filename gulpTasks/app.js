6509a5426fb3fef6817437e62f570c30e278d7a1
Akatchin <thiago.jp.221@gmail.com>
2023-01-07 20:03:37 -0300

    first commit

---
+++ gulpTasks/app.js
@@ -0,0 +1,25 @@
+const gulp = require("gulp")
+const { series } = require("gulp")
+
+function appHtml(cb){
+    return cb()
+}
+
+function appCSS(cb){
+    return cb()
+}
+
+function appJS(cb){
+    return cb()
+}
+
+function appIMG(cb){
+    return cb()
+}
+
+module.exports = {
+    appHtml,
+    appCSS,
+    appJS,
+    appIMG
+}
